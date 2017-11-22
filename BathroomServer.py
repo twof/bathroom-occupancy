c1d8647de2407c55df17480acdf93c206925ed89
palexis3 <alexispatrick77@gmail.com>
2017-11-22 05:04:56 -0800

    Freezed gunicorn file

--- BathroomServer.py
+++ BathroomServer.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask, request, jsonify
 from collections import deque
 import Person
 import datetime
