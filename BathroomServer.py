a4b0cd3fa76b2b8088b19331f5172990f4ed540b
palexis3 <alexispatrick77@gmail.com>
2017-11-22 13:49:41 -0800

    Fix

--- BathroomServer.py
+++ BathroomServer.py
@@ -122,7 +122,6 @@ def bathroom_availability():
             return "response url empty"
 
 
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
