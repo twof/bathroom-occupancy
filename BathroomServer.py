8c59ee96b42dcfa5b5d10f50b6570e76c83dc42c
palexis3 <alexispatrick77@gmail.com>
2017-11-22 14:09:34 -0800

    Fix

--- BathroomServer.py
+++ BathroomServer.py
@@ -103,9 +103,12 @@ def bathroom_availability():
     if request.method == 'POST':
         response_url = request.form['response_url']
         response_token = request.form['token']
-        data = {'text': 'Bathroom available!'}
-        if bathroomOccupied:
+        data = {}
+        if not bathroomOccupied:
+            data = {'text': 'Bathroom available!'}
+        else:
             data = {'text': 'Bathroom not available!'}
+
         if response_url != '' and response_token == token:
             r = requests.post(response_url, jsonify(data))
             if r.status_code != requests.codes.ok:
